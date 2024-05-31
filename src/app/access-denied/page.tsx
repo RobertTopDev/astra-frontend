1eb12db28ace982062c40c12dfadb7688d9b2b84
RobertTopdev <roberttanelriegler@gmail.com>
2024-05-31 07:17:30 -0400

    created access denied page

---
+++ src/app/access-denied/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AccessDeniedPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Access Denied</h1>
+      <p>Your access to this site has been restricted.</p>
+    </div>
+  )
+}
+
+export default AccessDeniedPage
--- src/components/ClientIPFetcher.tsx
+++ src/components/ClientIPFetcher.tsx
@@ -1,16 +1,20 @@
 'use client'
 
 import { usePathname, useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ClientIPFetcher: React.FC = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchIP = async () => {
       try {
-        if (pathname === '/access-denied') return
+        if (pathname === '/access-denied') {
+          setLoading(false)
+          return
+        }
 
         const response = await fetch('https://jsonip.com')
         const data = await response.json()
@@ -19,14 +23,23 @@ const ClientIPFetcher: React.FC = () => {
           `${process.env.NEXT_PUBLIC_API_URL}/checkip?clientIp=${ip}`
         )
 
-        if (serverResponse.status > 400) router.push('/access-denied')
+        if (serverResponse.status > 400) {
+          router.push('/access-denied')
+        } else {
+          setLoading(false)
+        }
       } catch (err) {
         console.error('Error fetching IP: ', err)
+        setLoading(false)
       }
     }
 
     fetchIP()
   }, [pathname])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return null
 }
