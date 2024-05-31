fa33d087e693884feae44a473253f600bff39e6c
RobertTopdev <roberttanelriegler@gmail.com>
2024-05-31 10:24:33 -0500

    add style of access denied page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { AstraRouterBack, Footer, Navbar } from '@/components'
 import '@rainbow-me/rainbowkit/styles.css'
 import clsx from 'clsx'
@@ -9,6 +11,7 @@ import { Providers } from './providers'
 import Loading from './loading'
 import localFont from 'next/font/local'
 import ClientIPFetcher from '@/components/ClientIPFetcher'
+import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -49,6 +52,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
   return (
     <html lang="en">
       <body
@@ -64,7 +68,7 @@ export default function RootLayout({
                 {children}
               </div>
             </Suspense>
-            <Footer />
+            {pathname !== '/access-denied' && <Footer />}
           </Providers>
         </Suspense>
       </body>
