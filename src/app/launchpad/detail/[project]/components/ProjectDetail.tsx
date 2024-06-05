da61f91917af6f0907fca439f88a196bb6699338
RobertTopdev <roberttanelriegler@gmail.com>
2024-06-05 09:05:10 -0400

    chore: Add new options to project category select menu

--- src/app/launchpad/detail/[project]/components/ProjectDetail.tsx
+++ src/app/launchpad/detail/[project]/components/ProjectDetail.tsx
@@ -1517,6 +1517,10 @@ export default function ProjectDetail({ data, refetchData }: Props) {
                             <SelectItem value="gaming">Gaming</SelectItem>
                             <SelectItem value="metaverse">Metaverse</SelectItem>
                             <SelectItem value="defi">DeFi</SelectItem>
+                            <SelectItem value="dex">DEX</SelectItem>
+                            <SelectItem value="cex">CEX</SelectItem>
+                            <SelectItem value="meme">Meme</SelectItem>
+                            <SelectItem value="ai">AI</SelectItem>
                             <SelectItem value="socialNetwork">
                               Social Network
                             </SelectItem>
--- src/app/launchpad/request/sections/create-form/index.tsx
+++ src/app/launchpad/request/sections/create-form/index.tsx
@@ -1471,6 +1471,10 @@ const CreateForm = () => {
                       <SelectItem value="gaming">Gaming</SelectItem>
                       <SelectItem value="metaverse">Metaverse</SelectItem>
                       <SelectItem value="defi">DeFi</SelectItem>
+                      <SelectItem value="dex">DEX</SelectItem>
+                      <SelectItem value="cex">CEX</SelectItem>
+                      <SelectItem value="meme">Meme</SelectItem>
+                      <SelectItem value="ai">AI</SelectItem>
                       <SelectItem value="socialNetwork">
                         Social Network
                       </SelectItem>
