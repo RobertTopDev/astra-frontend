5cd781ebb467b379245db0ad03f3b29a246c3a2c
RobertTopdev <roberttanelriegler@gmail.com>
2024-06-12 04:38:24 -0400

    chore: Update Unlocks component to handle SALE_ROUND_DETAIL split and rendering

--- src/app/launchpad/detail/[project]/components/Unlocks.tsx
+++ src/app/launchpad/detail/[project]/components/Unlocks.tsx
@@ -25,29 +25,28 @@ export default function Unlocks({ launchpadDetail }: TComponent) {
           </div>
         </div>
       </div>
-      <div className="p-8 rounded-xl bg-gradient-to-r from-[#636389] to-[#2C2C51] shadow-xl">
-        {launchpadDetail.SALE_ROUND_DETAIL
-          ? launchpadDetail.SALE_ROUND_DETAIL?.split('<>').map((item, idx) => (
-              <div
-                className="flex flex-col justify-center text-white"
-                key={idx}
-              >
-                <div className="text-md">{item.split(':')[3]}</div>
-                <div className="md:text-xl text-md font-medium">
-                  {item.split(':')[2]}
-                </div>
+      {launchpadDetail.SALE_ROUND_DETAIL ? (
+        launchpadDetail.SALE_ROUND_DETAIL?.split('<>').map((item, idx) => (
+          <div
+            className="p-8 rounded-xl bg-gradient-to-r from-[#636389] to-[#2C2C51] shadow-xl"
+            key={idx}
+          >
+            <div className="flex flex-col justify-center text-white">
+              <div className="text-md">{item.split(':')[3]}</div>
+              <div className="md:text-xl text-md font-medium">
+                {item.split(':')[2]}
               </div>
-            ))
-          : 'N/A'}
-        {/* <div className="flex flex-col justify-center h-full text-white">
-          <div className="text-md">Private Sale</div>
-          <div className="md:text-xl text-md font-medium">
-            {launchpadDetail.SALE_ROUND_DETAIL
-              ? launchpadDetail.SALE_ROUND_DETAIL.split('<>')[0].split(':')[2]
-              : 'N/A'}
+            </div>
           </div>
-        </div> */}
-      </div>
+        ))
+      ) : (
+        <div className="p-8 rounded-xl bg-gradient-to-r from-[#636389] to-[#2C2C51] shadow-xl">
+          <div className="flex flex-col justify-center text-white">
+            <div className="text-md"></div>
+            <div className="md:text-xl text-md font-medium">N/A</div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/app/launchpad/update/[launchpad_id]/page.tsx
+++ src/app/launchpad/update/[launchpad_id]/page.tsx
@@ -795,11 +795,10 @@ export default function Page({ params }: TPage) {
   }, [launchpadDetail])
   const { data: buyRuleStatus } = useGetBuyRuleLaunchpad() // [0]: whitelisted result
 
-  const { data: baseTokenDecimals, isLoading: baseTokenDecimalsLoading } =
-    useDecimals({
-      address: launchpadDetail?.BASE_TOKEN as `0x${string}`,
-      enabled: !!launchpadDetail,
-    })
+  const { data: baseTokenDecimals } = useDecimals({
+    address: launchpadDetail?.BASE_TOKEN as `0x${string}`,
+    enabled: !!launchpadDetail,
+  })
 
   // request launchpad after approve
   const {
@@ -937,9 +936,9 @@ export default function Page({ params }: TPage) {
     value_temp.leadVCImage = launchpadDetail?.LEAD_VC_IMAGE || ''
     value_temp.marketMakerImage = launchpadDetail?.MARKET_MAKER_IMAGE || ''
     value_temp.investorDetail = JSON.stringify(
-      launchpadDetail?.INVESTOR_DETAIL?.split(',')?.map((investor: string) =>
-        investor.trim()
-      )
+      value_temp?.investorDetail
+        ?.split(',')
+        ?.map((investor: string) => investor.trim())
     )
 
     const result_values: RequestLaunchpadResultValues = {
