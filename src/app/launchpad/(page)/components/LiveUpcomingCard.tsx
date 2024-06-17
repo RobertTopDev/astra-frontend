47bb550831472760e84bb6c8d309b98694326c5c
RobertTopdev <roberttanelriegler@gmail.com>
2024-06-17 11:07:52 +0100

    fix hardcap -> hard cap (soft cap also)

--- src/app/launchpad/(page)/components/LiveUpcomingCard.tsx
+++ src/app/launchpad/(page)/components/LiveUpcomingCard.tsx
@@ -444,7 +444,7 @@ export default function LiveUpcomingCard({
           </div>
         </CardHeader>
         <div className="relative flex w-full flex-col items-stretch p-6">
-          <div className="text-white text-sm font-black">Softcap - Hardcap</div>
+          <div className="text-white text-sm font-black">Soft Cap - Hard Cap</div>
           <div
             className="text-astra-blue text-xl tracking-[2px] mt-2"
             style={{
